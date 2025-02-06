b05edf78180ff3a11a4b57871fae916244be19ec
hinata <ryugamine020401@gmail.com>
2025-02-06 16:43:55 +0000

    feat(backend): 在 UsersModel 中新增驗證碼欄位
    - 修改 `UsersModel.py`，新增驗證碼欄位以支援用戶驗證機制。
    - 允許儲存並驗證使用者註冊或密碼重設時的驗證碼。
    
    此變更增強了使用者驗證流程，提升帳戶安全性。

--- backend/models/UsersModel.py
+++ backend/models/UsersModel.py
@@ -12,6 +12,7 @@ class UsersModel(Model):
     username = fields.CharField(max_length=30, unique=True)
     email = fields.CharField(max_length=320, unique=True)
     password = fields.CharField(max_length=255)
+    verify_num = fields.CharField(max_length=36)
     created_at = fields.CharField(max_length=25)
     updated_at = fields.CharField(max_length=36)
 
