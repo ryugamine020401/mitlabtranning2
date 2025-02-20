21dcd376c1c52f96cb33c853d03993f37e359abe
yihua.cai135 <jess92725@gmail.com>
2025-02-20 09:09:24 +0000

    fix: 修正 "get_story 需要登入" 功能

--- backend/actors/GPTManager.py
+++ backend/actors/GPTManager.py
@@ -6,34 +6,32 @@ router = APIRouter()
 class GPTManager:
     @staticmethod
     @router.post("/get_story/")
-    async def get_story(data: GetStory):
+    async def get_story(data: GetStory, current_user: UsersModel = Depends(get_current_user)):
         """
         創建有趣故事
         """
         try:
-                print("start...")
-                client = genai.Client(api_key=API_KEY)
-                response = client.models.generate_content(
-                    model="gemini-2.0-flash",             
-                    contents=generate_prompt(data.product_name, data.expiry_date),
-                    config={
-                        'response_mime_type': 'application/json',
-                        'response_schema': list[ReturnStory],
-                    }
-                )
-                print(f"Response: {response.text}")
-                my_story: list[ReturnStory] = response.parsed
-                return {
-                    "status": "success", 
-                    "msg": "Successful create a story.", 
-                    "data": [
-                        {
-                            "title": story.title, 
-                            "story": story.paragraph
-                        } 
-                        for story in my_story
-                    ]
+            client = genai.Client(api_key=API_KEY)
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",             
+                contents=generate_prompt(data.product_name, data.expiry_date),
+                config={
+                    'response_mime_type': 'application/json',
+                    'response_schema': list[ReturnStory],
                 }
+            )
+            my_story: list[ReturnStory] = response.parsed
+            return {
+                "status": "success", 
+                "msg": "Successful create a story.", 
+                "data": [
+                    {
+                        "title": story.title, 
+                        "story": story.paragraph
+                    } 
+                    for story in my_story
+                ]
+            }
             
         except Exception as e:
-            return {"status": "fail", "msg": "Fail to create a story.", "data": [], "e": str(e)}
+            return {"status": "fail", "msg": "Fail to create a story.", "data": []}
